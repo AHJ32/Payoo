db8e10d8201565b7c4678f14c0882a2243c8ecb4
AHJ32 <jihadanamulhaque9@gmail.com>
2026-03-17 14:04:26 +0600

    Cash Out section is functional

--- JS/cashout.js
+++ JS/cashout.js
@@ -1,25 +1,34 @@
 // Credentials
-import { accountNumber } from "./config.js";
-import { pin } from "./config.js";
+import { accountNumber, pin } from "./config.js";
 
+//Error sound
+const errorSound = new Audio("Viral FAAH Sound.mp3");
 document.getElementById("withdraw-btn").addEventListener("click", () => {
-  //Error sound
-  const errorSound = new Audio("Viral FAAH Sound.mp3");
-  // Agent number fetch and validate
+  // Agent number fetch
   const agentNumber = document.getElementById("input-agent").value;
+  // amount fetch
+  const amount = Number(document.getElementById("input-amount").value);
+  //Current balance fetch
+  let currentBalance = Number(document.getElementById("balance").innerText);
+  //Pin fetch
+  const cashoutPin = document.getElementById("input-cashout-pin").value;
+  //Agent number validation
   if (agentNumber == accountNumber) {
     errorSound.play();
+    return;
+  }
+  // amount validation
+  if (amount <= 0 || amount > currentBalance) {
+    errorSound.play();
+    return;
+  }
+  //Pin validation
+  if (cashoutPin !== pin) {
+    errorSound.play();
   } else {
-    // amount fetch and validate
-    const amount = document.getElementById("input-amount").value;
-    const currentBalance = document.getElementById("balance").innerText;
-    if (Number(amount) >= Number(currentBalance)) {
-      errorSound.play();
-    } else {
-      const cashoutPin = document.getElementById("input-cashout-pin").value;
-      if (cashoutPin == pin) {
-        alert("Correct");
-      }
-    }
+    //calc
+    const updatedBalance = currentBalance - amount;
+    //Updating the new balance
+    document.getElementById("balance").innerText = updatedBalance;
   }
 });
